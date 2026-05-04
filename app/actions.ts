b0d6ff73d685d7af86f04992a0bcd46107abc58e
sohilalotfy000-glitch <sohilalotfy000@gmail.com>
2026-05-04 15:53:37 +0300

    Update actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -7,17 +7,12 @@ import { revalidatePath } from "next/cache";
 export async function saveProgress(tasks: any, totalXP: number, uId: string, uName: string, uPic: string) {
   if (!uId) return;
   try {
-    // هنجيب القائمة القديمة
     let board: any = (await kv.get("quran_board_final")) ||[];
     if (!Array.isArray(board)) board =[];
     
-    // هنمسح اسمك القديم لو موجود عشان نحدثه بالنقط الجديدة
     board = board.filter((user: any) => user.id !== uId);
-    
-    // هنضيف اسمك ونقطك الجديدة
     board.push({ id: uId, name: uName || "طالب", avatar: uPic || "", xp: totalXP });
     
-    // هنحفظ القائمة كلها
     await kv.set("quran_board_final", board);
     revalidatePath("/");
   } catch (e) {
@@ -30,11 +25,10 @@ export async function getDashboardData() {
     let board: any = (await kv.get("quran_board_final")) ||[];
     if (!Array.isArray(board)) board =[];
     
-    // ترتيب من الكبير للصغير
     board.sort((a: any, b: any) => b.xp - a.xp);
     return { leaderboard: board.slice(0, 10) };
   } catch (e) {
-    return { leaderboard:
-
-
+   return { leaderboard: [ ] };
+  }
+}
 
